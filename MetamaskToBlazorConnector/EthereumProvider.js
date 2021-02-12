062807452ce3ab72b5eed6b40366ce71c1e876e2
argon666 <konrad.korczak18@gmail.com>
2021-02-12 17:31:24 +0100

    Create EthereumProvider.js
    JavaScript file to communicate with ethereum provider

---
+++ MetamaskToBlazorConnector/EthereumProvider.js
@@ -0,0 +1,22 @@
+﻿window.EthereumProvider = {
+    IsAvailable: () => {
+        if (window.ethereum) return true;
+        return false;
+    },
+    ReturnAccount: async () => {
+        try {
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            return accounts[0];
+        } catch (error) {
+            return null;
+        }
+
+    },
+    SendMessage: async (message) => {
+        return new Promise(function (resolve, reject) {
+            ethereum.send(JSON.parse(message), function (error, result) {
+                resolve(JSON.stringify(result));
+            });
+        });
+    }
+}
